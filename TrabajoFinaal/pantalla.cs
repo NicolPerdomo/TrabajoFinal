eed1f2f582484cef7fd42d6f67912b2c6552759c
nicol <dayahiguera@hotmail.com>
2021-04-13 03:20:05 -0500

    taller

---
+++ TrabajoFinaal/pantalla.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrabajoFinaal
+{
+    class pantalla
+    {
+        public void pantalla1()
+        {
+            for (int i = 1; i < 110; i++)
+            {
+                Console.SetCursorPosition(i, 0); Console.Write("▒");
+                Console.SetCursorPosition(i, 6); Console.Write("═");
+                Console.SetCursorPosition(i, 9); Console.Write("═");
+                Console.SetCursorPosition(i, 22); Console.Write("═");
+                Console.SetCursorPosition(i, 25); Console.Write("▒");
+
+            }
+            for (int i = 0; i <= 25; i++)
+
+            {
+                Console.SetCursorPosition(1, i); Console.Write("░");
+                Console.SetCursorPosition(110, i); Console.Write("░");
+
+            }
+
+            Console.SetCursorPosition(60, 13); Console.Write(" BIENVENIDOS ");
+            Console.SetCursorPosition(10, 15); Console.Write(" ▓▓▓▓▓ ▓▓▓▓▓▓ ▓▓     ▓   ▓▓▓  ");
+            Console.SetCursorPosition(10, 16); Console.Write("▓      ▓      ▓ ▓    ▓  ▓   ▓ ");
+            Console.SetCursorPosition(10, 17); Console.Write("▓      ▓      ▓  ▓   ▓ ▓     ▓");
+            Console.SetCursorPosition(10, 18); Console.Write(" ▓▓▓▓  ▓▓▓▓▓  ▓   ▓  ▓ ▓▓▓▓▓▓▓");
+            Console.SetCursorPosition(10, 19); Console.Write("     ▓ ▓      ▓    ▓ ▓ ▓     ▓");
+            Console.SetCursorPosition(10, 20); Console.Write("     ▓ ▓      ▓     ▓▓ ▓     ▓");
+            Console.SetCursorPosition(10, 21); Console.Write("▓▓▓▓▓  ▓▓▓▓▓▓ ▓      ▓ ▓     ▓");
+            Console.SetCursorPosition(10, 23); Console.WriteLine("Centro de Gestión de Mercados, Logística y Tecnologías de la Información");
+        }
+
+
+
+        public void pantalla2()
+        {
+            for (int i = 35; i <= 80; i++)
+            {
+                Console.SetCursorPosition(i, 0); Console.Write("▒");
+                //Console.SetCursorPosition(i, 2); Console.Write("═");
+                //Console.SetCursorPosition(i, 18); Console.Write("═");
+                Console.SetCursorPosition(i, 35); Console.Write("▒");
+
+
+            }
+            for (int i = 0; i <= 35; i++)
+
+            {
+                Console.SetCursorPosition(35, i); Console.Write("░");
+                Console.SetCursorPosition(80, i); Console.Write("░");
+
+            }
+
+        }
+
+        public void pantalla3()
+        {
+            for (int i = 1; i < 80; i++)
+            {
+                Console.SetCursorPosition(i, 0); Console.Write("▒");
+                Console.SetCursorPosition(i, 6); Console.Write("═");
+                //Console.SetCursorPosition(i, 9); Console.Write("═");
+                //Console.SetCursorPosition(i, 22); Console.Write("═");
+                Console.SetCursorPosition(i, 25); Console.Write("▒");
+
+            }
+            for (int i = 0; i <= 25; i++)
+
+            {
+                Console.SetCursorPosition(1, i); Console.Write("░");
+                Console.SetCursorPosition(80, i); Console.Write("░");
+
+            }
+
+        }
+    }
+}
---
+++ TrabajoFinaal/validaciones.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TrabajoFinal
+{
+    class validaciones
+    {
+        public bool Vacio(string texto)
+        {
+            if (texto.Equals(""))
+            {
+                Console.WriteLine(" La entrada no puede ser VACIO");
+                return true;
+            }
+            else
+                return false;
+        }
+        public bool TipoNumero(string texto)
+        {
+            Regex regla = new Regex("[0-9]{1,9}(\\.[0-9]{0,2})?$");
+
+            if (regla.IsMatch(texto))
+                return true;
+            else
+            {
+                Console.WriteLine(" La entrada no debe ser NUMERICA");
+                return false;
+            }
+        }
+        public bool TipoTexto(string texto)
+        {
+            Regex regla = new Regex("^[a-zA-Z ]*$");
+
+            if (regla.IsMatch(texto))
+                return true;
+            else
+            {
+                Console.SetCursorPosition(35,18);
+                Console.WriteLine(" La entrada debe ser SOLO TEXTO");
+                return false;
+       
+            }
+     
+        }
+
+        public bool TipoCorreo(string texto)
+        {
+            Regex regla = new Regex(@"\A(\w+\.?\w*\@\w+\.)(com)\Z");
+
+            if (regla.IsMatch(texto))
+                return true;
+            else
+            {
+                Console.WriteLine(" La entrada debe ser correo electronico");
+                return false;
+            }
+        }
+
+
+
+    }
+}
+
+
